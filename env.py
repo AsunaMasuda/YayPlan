acd2717eb96ee04680f165c9fd6c2ed2b1792562
Asuna Masuda <asuna.fitz@gmail.com>
2020-05-12 21:33:00 +0000

    Change env.py file

--- env.py
+++ env.py
@@ -1,3 +1,3 @@
 import os
 
-os.environ.setdefault("MONGO_URI", "mongodb+srv://root:r00tUser@myfirstcluster-1nj1f.mongodb.net/myPlanner≈?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://root:r00tUser@myfirstcluster-1nj1f.mongodb.net/myPlanner?retryWrites=true&w=majority")
