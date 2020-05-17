a88d6439559401526475b5656dffe4d219b6df5d
Asuna Masuda <asuna.fitz@gmail.com>
2020-05-17 00:24:57 +0000

    Add function that enables to remove the input of places in create_new_plan.html

--- static/main.js
+++ static/main.js
@@ -1,41 +1,70 @@
-$(document).ready(function(){
-    
-// Add more places 
-/**
- * This function adds more options for places
- * @constructor
- */
-$("#add_places").on("click", function () {
-  $("#extra_places").append(
-    `<input type="text"
-    class="form-control event_place"
-    name="event_place` + ((Number($('.event_place').length)) + 1) + `"/>`
-)
-});
+$(document).ready(function () {
+  // Add more places in create_new_plan.html
+  /**
+   * This function adds more options for places
+   * @constructor
+   */
+  $("#addplaces_button").on("click", function () {
+    // Finding total number of elements added
+    let total_element = $(".element").length;
+    let lastid = $(".element:last").attr("id");
+    let split_id = lastid.split("_");
+    let next_index = Number(split_id[1]) + 1;
 
-// Add more date & time
-/**
- * This function adds more options for date and time
- * @constructor
- */
-$("#add_avail").on("click", function () {
-$("#taskEmpty").append(`<input
+    let max = 10;
+    if (total_element < max) {
+      // Adding new div container after last occurance of element class
+      $(".element:last")
+        .after(`<div class="element" id="addplaces_` + next_index + `"></div>`)
+        
+       $("#addplaces_" + next_index).append(
+          `<div class="row"><div class="col-sm-9"><input type="text" class="form-control event_place" name="event_place` +
+            next_index + `"></div><div class="col-sm-3 my-auto"><span id="remove_` +
+            next_index +
+            `" class="remove_place_avail">Remove</span></div></div>`
+        );
+    }
+  });
+
+  // Remove element
+  $(".container").on("click", ".remove_place_avail", function () {
+    let remove_id = this.id;
+    let split_id = remove_id.split("_");
+    let deleteindex = split_id[1];
+
+    $('#addplaces_' + deleteindex).remove();
+  });
+
+
+  // Add more date & time in create_new_plan.html
+  /**
+   * This function adds more options for date and time
+   * @constructor
+   */
+  $("#add_avail").on("click", function () {
+    $("#taskEmpty")
+      .append(
+        `<input
                   type="text"
                   class="form-control avail_picker"
-                  name="event_availability` + (Number($('#event_availability').length) + 1) + `"
-                />`).find(".avail_picker").flatpickr(fpConf);
+                  name="event_availability` +
+          (Number($("#event_availability").length) + 1) +
+          `"
+                />`
+      )
+      .find(".avail_picker")
+      .flatpickr(fpConf);
+  });
+
+  // Add flatpickr
+  /**
+   * This function enables flatpickr
+   * @constructor
+   */
+  const fpConf = {
+    enableTime: true,
+    dateFormat: "Y-m-d Kh:i",
+  };
+
+  flatpickr(".avail_picker", fpConf);
 });
-
-// Add flatpickr 
-/**
- * This function enables flatpickr
- * @constructor
- */
-const fpConf = {
-  enableTime: true,
-  dateFormat: "Y-m-d Kh:i",
-};
-
-flatpickr(".avail_picker", fpConf);
-
-})
