1458a57191ac70f0ec6a325ed326c0be2c6dd3f6
Asuna Masuda <asuna.fitz@gmail.com>
2020-05-17 09:34:10 +0000

    Add remove button to date & time input area

--- static/main.js
+++ static/main.js
@@ -6,28 +6,28 @@ $(document).ready(function () {
    */
   $("#addplaces_button").on("click", function () {
     // Finding total number of elements added
-    let total_element = $(".element").length;
-    let lastid = $(".element:last").attr("id");
+    let total_element = $(".element_place").length;
+    let lastid = $(".element_place:last").attr("id");
     let split_id = lastid.split("_");
     let next_index = Number(split_id[1]) + 1;
 
     let max = 10;
     if (total_element < max) {
       // Adding new div container after last occurance of element class
-      $(".element:last")
-        .after(`<div class="element" id="addplaces_` + next_index + `"></div>`)
+      $(".element_place:last")
+        .after(`<div class="element_place" id="addplaces_` + next_index + `"></div>`)
         
        $("#addplaces_" + next_index).append(
           `<div class="row"><div class="col-sm-9"><input type="text" class="form-control event_place" name="event_place` +
             next_index + `"></div><div class="col-sm-3 my-auto"><span id="remove_` +
             next_index +
-            `" class="remove_place_avail">Remove</span></div></div>`
+            `" class="remove_place_button">Remove</span></div></div>`
         );
     }
   });
 
   // Remove element
-  $(".container").on("click", ".remove_place_avail", function () {
+  $(".container").on("click", ".remove_place_button", function () {
     let remove_id = this.id;
     let split_id = remove_id.split("_");
     let deleteindex = split_id[1];
@@ -41,19 +41,35 @@ $(document).ready(function () {
    * This function adds more options for date and time
    * @constructor
    */
-  $("#add_avail").on("click", function () {
-    $("#taskEmpty")
-      .append(
-        `<input
-                  type="text"
-                  class="form-control avail_picker"
-                  name="event_availability` +
-          (Number($("#event_availability").length) + 1) +
-          `"
-                />`
-      )
-      .find(".avail_picker")
-      .flatpickr(fpConf);
+  $("#addavail_button").on("click", function () {
+    // Finding total number of elements added
+    let total_element = $(".element_avail").length;
+    let lastid = $(".element_avail:last").attr("id");
+    let split_id = lastid.split("_");
+    let next_index = Number(split_id[1]) + 1;
+
+    let max = 10;
+    if (total_element < max) {
+      // Adding new div container after last occurance of element class
+      $(".element_avail:last")
+        .after(`<div class="element_avail" id="addavail_` + next_index + `"></div>`)
+        
+       $("#addavail_" + next_index).append(
+          `<div class="row"><div class="col-sm-9"><input type="text" class="form-control avail_picker" name="event_availability` +
+            next_index + `"></div><div class="col-sm-3 my-auto"><span id="removeavail_` +
+            next_index +
+            `" class="remove_avail_button">Remove</span></div></div>`
+        ).find(".avail_picker").flatpickr(fpConf);
+    }
+  });
+
+  // Remove element
+  $(".container").on("click", ".remove_avail_button", function () {
+    let remove_id = this.id;
+    let split_id = remove_id.split("_");
+    let deleteindex = split_id[1];
+
+    $('#addavail_' + deleteindex).remove();
   });
 
   // Add flatpickr
