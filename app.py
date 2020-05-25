a5c7540085a227c7b424e36f5664fa4d14a3f511
Asuna Masuda <asuna.fitz@gmail.com>
2020-05-25 23:37:49 +0000

    Add function for editting in app.py

--- app.py
+++ app.py
@@ -35,7 +35,8 @@ def restore_plan():
     range_availability = range(0, len(the_plan['availabilities']))
     if the_plan['participants'] == 0:
         range_participant = 0
-    else: range_participant = range(0, len(the_plan['participants']))
+    else:
+        range_participant = range(0, len(the_plan['participants']))
     return render_template('update_plan_participants.html', plan_id=plan_id, the_plan=the_plan, range_participant=range_participant, range_availability=range_availability)
 
 
@@ -55,8 +56,7 @@ def update_details(plan_id):
                      'event_description': request.form["event_description"],
                      'availabilities': list_avail,
                      'event_place': request.form["event_place"],
-                     'participants': [],
-                     'theme': request.form["theme"]
+                     'participants': []
                  }})
     return render_template('after_creating_plan.html', plan_id=plan_id)
 
@@ -67,7 +67,8 @@ def update_plan_participants(plan_id):
     range_availability = range(0, len(the_plan['availabilities']))
     if the_plan['participants'] == 0:
         range_participant = 0
-    else: range_participant = range(0, len(the_plan['participants']))
+    else:
+        range_participant = range(0, len(the_plan['participants']))
     return render_template('update_plan_participants.html', plan_id=plan_id, the_plan=the_plan, range_participant=range_participant, range_availability=range_availability)
 
 
@@ -77,26 +78,27 @@ def update_plan_complete(plan_id):
     while i < 50:
         i = i + 1
         each_participant = "participant_" + str(i)
-        try : 
+        try:
             name_participant = request.form[each_participant]
             dict_avail = []
             for n in range(1, 6):
-                participant_each_availability = "participant_" + str(i) + "_availability_" + str(n)
-                try :
-                   dict_avail.append(request.form[participant_each_availability])
+                participant_each_availability = "participant_" + \
+                    str(i) + "_availability_" + str(n)
+                try:
+                    dict_avail.append(
+                        request.form[participant_each_availability])
                 except:
                     break
             the_plan = mongo.db.plans
             the_plan.update({'_id': ObjectId(plan_id)},
-            { '$push' : {
-                "participants" : {
-                'name': name_participant,
-                'availabilities' : dict_avail,
-                'participant_note': request.form["participant_note"]
-            }}
-            })
-        except: 
-            continue
+                            {'$push': {
+                                "participants": {
+                                    'name': name_participant,
+                                    'availabilities': dict_avail,
+                                    'participant_note': request.form["participant_note"]
+                                }}
+                             })
+        except: continue
 
     return render_template('after_updating_plan.html', plan_id=plan_id)
 
@@ -107,8 +109,39 @@ def check_plan_participants(plan_id):
     range_availability = range(0, len(the_plan['availabilities']))
     if the_plan['participants'] == 0:
         range_participant = 0
-    else: range_participant = range(0, len(the_plan['participants']))
+    else:
+        range_participant = range(0, len(the_plan['participants']))
     return render_template('update_plan_participants.html', plan_id=plan_id, the_plan=the_plan, range_participant=range_participant, range_availability=range_availability)
+
+
+@app.route('/edit_yourplan/<plan_id>', methods=["POST"])
+def edit_yourplan(plan_id):
+    the_plan = mongo.db.plans.find_one({'_id': ObjectId(plan_id)})
+    i = 0
+    while i < len(the_plan['participants']):
+        if request.form['edit_name'] ==  the_plan['participants'][i]['name']:
+            dict_avail_edit = []
+            for n in range(1, 6):
+                each_availability = "availability_" + str(n)
+                try:
+                    dict_avail_edit.append(request.form[each_availability])
+                except:
+                    break
+            updating_participant_DB = "participants." + str(i)
+
+            mongo.db.plans.update(
+                {'_id': ObjectId(plan_id)},
+                    {'$set': {
+                        updating_participant_DB : {
+                            'name': request.form['edit_name'],
+                            'availabilities': dict_avail_edit,
+                            'participant_note': request.form["participant_note"]
+                        }}})
+            i = i + 1
+        else: 
+            i = i + 1
+            continue
+    return render_template('after_updating_plan.html', plan_id=plan_id)
 
 
 if __name__ == '__main__':
